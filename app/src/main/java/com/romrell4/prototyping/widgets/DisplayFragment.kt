6ceb3a2d53634a00a3bb4383c130650774359144
Eric Romrell <eric_romrell@byu.edu>
2019-05-02 19:25:40 -0600

    Initial project

---
+++ app/src/main/java/com/romrell4/prototyping/widgets/DisplayFragment.kt
@@ -0,0 +1,16 @@
+package com.romrell4.prototyping.widgets
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.romrell4.prototyping.R
+
+class DisplayFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.fragment_display, container, false).apply {
+
+        }
+}
